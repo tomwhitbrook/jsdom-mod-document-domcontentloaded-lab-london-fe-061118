be286172af4aa27d64b0f8ea0d4736167b281987
Tom Whitbrook <tomwhitbrook@gmail.com>
2018-07-30 19:11:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 /* code goes here */
+
+document.addEventListener("DOMContentLoaded", e => {
+  document.querySelector("body")
+   .addEventListener("click", e => console.log("Reggae, Reggae!"));
+})
