040a31b8902546f7ee3546d7d40e626a5eb1ffd9
Tom Whitbrook <tomwhitbrook@gmail.com>
2018-07-30 19:26:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 /* code goes here */
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  document.getElementById("hidden-div").innerText;
-  console.log(innerText)
+  document.getElementById("hidden-div").innerText = 301;
 })
